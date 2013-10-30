bf20fa65357fc6b75add8e2f94f733ed48d80490
Igor <igor.kh.mail@gmail.com>
2013-10-30 15:55:03 +0200

    unneded params removed

--- WhiteLabelApp/Settings/WLAGlobalSettings.h
+++ WhiteLabelApp/Settings/WLAGlobalSettings.h
@@ -6,17 +6,12 @@ extern "C" {
         
     extern NSString* const WLAWebApiHostName;
     extern NSString* const WLASitecoreShellSite;
-    extern NSString* const WLAWebApiHostNameWithAuth;
         
     extern NSString* const WLAUserName;
     extern NSString* const WLAUserPassword;
         
     extern NSString* const WLAHomeItemId;
     extern NSString* const WLAWhiteLabelDataSourceRoot;
-    
-    
-    extern NSString* const WLACreatorUser;
-    extern NSString* const WLACreatorPassword;
     
 #ifdef __cplusplus
 }
--- WhiteLabelApp/Settings/WLAGlobalSettings.m
+++ WhiteLabelApp/Settings/WLAGlobalSettings.m
@@ -1,10 +1,8 @@
 #import "WLAGlobalSettings.h"
 
-#error  posible error, correct settings shoul be used!
+//#error  posible error, correct settings shoul be used!
 NSString* const WLAWebApiHostName = @"http://ws-alr1.dk.sitecore.net:70/";
 NSString* const WLASitecoreShellSite = @"/sitecore/shell";
-
-NSString* const WLAWebApiHostNameWithAuth = @"http://mobiledev1ua1.dk.sitecore.net:9999";
 
 NSString* const WLAUserName = @"admin";
 NSString* const WLAUserPassword = @"b";
@@ -13,6 +11,4 @@ NSString* const WLAHomeItemId = @"{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}";
 
 NSString* const WLAWhiteLabelDataSourceRoot = @"/sitecore/content/WhiteLabelApplication/";
 
-NSString* const WLACreatorUser     = @"sitecore\\creator";
-NSString* const WLACreatorPassword = @"creator";
 
