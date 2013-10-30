eff3ff39da4de5a721724448435bfec8c6e5e646
Igor <igor.kh.mail@gmail.com>
2013-10-30 17:18:22 +0200

    settings values removed

--- WhiteLabelApp/Settings/WLAGlobalSettings.m
+++ WhiteLabelApp/Settings/WLAGlobalSettings.m
@@ -1,13 +1,13 @@
 #import "WLAGlobalSettings.h"
 
 //#error  posible error, correct settings shoul be used!
-NSString* const WLAWebApiHostName = @"http://ws-alr1.dk.sitecore.net:70/";
+NSString* const WLAWebApiHostName = @"<host>";
 NSString* const WLASitecoreShellSite = @"/sitecore/shell";
 
-NSString* const WLAUserName = @"admin";
-NSString* const WLAUserPassword = @"b";
+NSString* const WLAUserName = @"<username>";
+NSString* const WLAUserPassword = @"<password>";
 
-NSString* const WLAHomeItemId = @"{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}";
+NSString* const WLAHomeItemId = @"<home item id>";
 
 NSString* const WLAWhiteLabelDataSourceRoot = @"/sitecore/content/WhiteLabelApplication/";
 
